6b7440d9bdf8b27178f5c9225665b4c3a516cb79
柳滉平 <kyng718@icloud.com>
2026-04-24 15:24:36 +0900

    coutで画面出力できる

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,5 @@
 #include<print>
 #include<numbers>
 int main(int argc, char** argv) {
-	std::println("Hello World");
-	std::println("{:03d} {:+.5f} ", 3, std::numbers::pi);
+	std::cout << "Hello" << std::endl;
 }
