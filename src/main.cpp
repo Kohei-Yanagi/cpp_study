a9862d2e6279ea22b2a410d4d55fc4bc6403a2d9
柳滉平 <kyng718@icloud.com>
2026-04-26 19:46:00 +0900

    if文

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,17 @@
 #include<numbers>
 using namespace std;
 int main(int argc, char** argv) {
-	int x,y;
-	cout << "type the numbers: ";
-	cin >> x >> y;
-	cout << x + y<< "\n";
+	int num;
+	cout << "number:";
+	cin >> num; 
+	
+	if (num == 10)
+		cout << "true";
+	else if (num < 10)
+		cout << "small";
+	else if (num > 10)
+		cout << "big";
+	
+
+	return 0;
 }
