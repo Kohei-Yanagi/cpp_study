1bf793efdc4f39c972de8282f3de256f1eb79320
柳滉平 <kyng718@icloud.com>
2026-04-26 21:53:27 +0900

    二重のfor include<iomanip>で文字の幅を取れる cout << setw()を使う

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include<iostream>//プログラムの読み込み
+#include<iomanip>//文字の幅を取る
 #include<print>
 #include<numbers>
 using namespace std;
 int main(int argc, char** argv) {
-	for (int i = 0; i < 5; i++) {
-		cout << i << "times : message\n";
+	
+	int i, j;
+	for (i = 1; i <= 9; i++) {
+		    cout << i << "の段";
+		for (j = 1; j <= 9; j++) {
+			cout << setw(5) << i * j;
+			
+		}
+		cout << "\n";
 	}
-
-
-	
-	
 
 	return 0;
 }
